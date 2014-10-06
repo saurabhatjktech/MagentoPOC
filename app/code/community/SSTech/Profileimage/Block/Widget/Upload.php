d806bc6fe267db2ad74347fa1469eb6b23cfe957
Atul <atul@atul-ThinkCentre-M57e.(none)>
2014-10-06 18:22:47 +0530

    Merging code from local

--- app/code/community/SSTech/Profileimage/Block/Widget/Upload.php
+++
@@ -1,4 +0,0 @@
-<?php
-class SSTech_Profileimage_Block_Widget_Upload extends Mage_Core_Block_Template 
-            implements Mage_Widget_Block_Interface{ 
-} 
--- app/code/community/SSTech/Profileimage/Model/Config.php
+++
@@ -1,4 +0,0 @@
-<?php
-class SSTech_Profileimage_Model_Config{
-	const Profileimage_ATTR_CODE = 'sstech_profileimage';
-}
