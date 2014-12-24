0fd0ff2c074c2f3327c8de09938f02606bfa4762
saurabh kumar <saurabh@saurabh-ThinkCentre-M57e.(none)>
2014-12-24 16:34:28 +0530

    changes added till 241214

---
+++ app/code/local/Mage/CatalogRule/Block/Promotion/Promotions.php
@@ -0,0 +1,21 @@
+<?php 
+class Mage_CatalogRule_Block_Promotions extends Mage_CatalogRule_Model_Resource_Rule{
+    public function __construct(){
+        parent::__construct();
+        $storeId = Mage::app()->getStore()->getId();
+        $promotions = Mage::getResourceModel('catalogrule/promotion_collection')
+            ->addOrderedQty()
+            ->addAttributeToSelect('*')
+            ->addAttributeToSelect(array('name'))
+            ->setStoreId($storeId)
+            ->addStoreFilter($storeId);
+
+        echo "<pre>"; print_r($promotions); echo "</pre>"; die("HEREEEE");
+            
+        Mage::getSingleton('catalogrule/promotion_status')->addVisibleFilterToCollection($promotions);
+        Mage::getSingleton('catalogrule/promotion_visibility')->addVisibleInCatalogFilterToCollection($promotions);
+ 
+        $promotions->setPageSize(3)->setCurPage(1);
+        $this->setPromotionCollection($promotions);
+    }
+}
